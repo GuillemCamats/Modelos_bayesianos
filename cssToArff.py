ec8c33d8b8cf10d11d7748d4c0c671fe5f3864c3
GuillemCamats <guillemcamats@gmail.com>
2021-06-24 14:58:09 +0200

    comentat

--- cssToArff.py
+++ cssToArff.py
@@ -4,7 +4,33 @@ import sys
 import os
 import random
 
-def classify(list,learnig_path,testing_path,length,instances):
+# Aquesta funció es la que s'utilitza per escriure en els fitxers arff. Simplement obre els fitxers
+# i escriu el que correspont a cada fitxer. 
+
+def writeOnArff(path,nums,instances,file_list):
+    with open(path, "w") as file:
+        file.write("@RELATION file\n")
+        file.write("\n")
+
+        for i in range(len(instances)-1):
+            file.write("@ATTRIBUTE "+str(instances[i])+" {"+str(nums).replace(' ','')+"}\n")
+
+        file.write("@ATTRIBUTE Type {1,2,3,4,5,6,7}\n")
+        file.write("\n")
+        
+        file.write("@DATA\n")
+        for line in file_list:
+            line = str(line)[1 : -1]
+            file.write("%s\n" % line)
+            
+    file.close()
+
+
+# Aquesta funcio divideix les dades en dades de aprenentatge un 75% aproximadament, i la resta en dades
+# de testing. Aqui apliquem la funció seed amb els 5 digits finals dem meu DNI. Tot seguit cridem la 
+# funció que escriura ens els fitxers arff.
+
+def split7525(list,learnig_path,testing_path,argval,instances):
     learning_list = []
     testing_list = []
     random.seed(42287)
@@ -13,42 +39,17 @@ def classify(list,learnig_path,testing_path,length,instances):
             learning_list.append(sublist)
         else:
             testing_list.append(sublist)
-    nums = [ j+1 for j in range(int(length))]
+
+    nums = [ j+1 for j in range(int(argval))]
     nums = str(nums)[1 : -1]
-    with open(learnig_path, "w") as learning:
-        learning.write("@RELATION learning\n")
-        learning.write("\n")
-
-        for i in range(len(instances)-1):
-            learning.write("@ATTRIBUTE "+str(instances[i])+" {"+str(nums).replace(' ','')+"}\n")
-
-        learning.write("@ATTRIBUTE Type {1,2,3,4,5,6,7}\n")
-        learning.write("\n")
-        
-        learning.write("@DATA\n")
-        for line in learning_list:
-            line = str(line)[1 : -1]
-            learning.write("%s\n" % line)
-            
-    learning.close()
-
-    with open(testing_path, "w") as testing:
-        testing.write("@RELATION testing\n")
-        testing.write("\n")
-
-        for i in range(len(instances)-1):
-            testing.write("@ATTRIBUTE "+str(instances[i])+" {"+str(nums).replace(' ','')+"}\n")
-
-        testing.write("@ATTRIBUTE Type {1,2,3,4,5,6,7}\n")
-        testing.write("\n")
-        testing.write("@DATA\n")
-
-        for line in testing_list:
-            line = str(line)[1 : -1]
-            testing.write("%s\n" % line)
+    writeOnArff(learnig_path,nums,instances,learning_list)
+    writeOnArff(testing_path,nums,instances,testing_list)
     
-    testing.close()
 
+# Com es comenta en la funció openAndRead la llista amb la que treballem esta dividida amb sub llistes
+# on aquestes sub llistes el que representen son les col·lumnes del fitxer csv. El que fa aquesta funció
+# es agafar un valor de cada subllista i juntarlos amb una altra llista, d'aquesta manera tornem a tindre
+# una representacio del fitxer original. On cada subllista de la llista representara una fila del fitxer csv
 def orderLists(list):
     orderedList = []
     recicleList = []
@@ -60,30 +61,54 @@ def orderLists(list):
 
     return orderedList
 
+
+
+#Aquesta funció elimina els duplicats de una llista, es fa crean un diccionari i passant
+# els valor de la llista com a clau. Aixo elimina els duplicats perquè un diccionari no pot tindre
+# claus repetides. Finalment retornem el diccionari com a llista.
 def deleteDuplicates(x):
-  return list(dict.fromkeys(x))
+    return list(dict.fromkeys(x))
 
 
-def discreetDomain(atributs,length):
-    discreetList = [[] for _ in range(10)]
+
+# Aquesta funció es la que s'encarrega de transformar els valors reals a un rang discret.
+# Funciona de tal manera que s'agafen les subllistes per ordre, osigui les col·lumnes de el fitxer
+# csv, s'ordenen per valor i s'eliminen els valors repetits. Tot seguit els elements de la subllista 
+# seran dividits en tantes subsubllistes com el valor que ens donen per paramentre al executar el
+# script. D'aquesta manera cada subsub llista representara un valor del rang ente 1 i el nombre d'entrada.
+# Per assignar el el valor discret a un numero, el que es fa es deprés de tot lo anterior es mira si 
+# per cada element de la subllista original(la que no s'ha processat amb tot lo anterior) es mira a quina
+# subsub llista es troba ell mateix, ex: el numero 1.52101 de RI esta en la subllista 4, llavors 
+# 1.52101 passara a ser 4. Aquest numero 4 s'afegeix en una nova llista anomenada discret list
+# la qual al final contindra tots els valors reals transformats a discrets. 
+def discreetDomain(atributs,argval):
+    discreetList = [[] for _ in range(9)]
     i = 0
     for list in atributs:
         sortedList = list.copy()
-        sortedList.sort()
-        sortedList = deleteDuplicates(sortedList)
-        dividedList= [sortedList[i:i+int(len(sortedList)/int(length))+1] for i in range(0, len(sortedList), int(len(sortedList)/int(length))+1)]
+        sortedList.sort() # ordenar
+        sortedList = deleteDuplicates(sortedList) #eliminar repetits
+        # dividir la list en tantes subllistes com el numero argval
+        dividedList= [sortedList[i:i+int(len(sortedList)/int(argval))+1] for i in range(0, len(sortedList), int(len(sortedList)/int(argval))+1)]
         for elem in list:
             x = 1
-            for dlist in dividedList:
-                if elem in dlist:
-                    discreetList[i].append(x)
+            for dlist in dividedList: 
+                if elem in dlist:     # si el element pertany a la subllista assginem el valor x a el numero corresponent
+                    discreetList[i].append(x)  # sent x la subsub llista al qual pertany el element
                     break
                 else:
                     x +=1
         i+=1
     return discreetList
 
-def openAndCsstoArff(css_path):
+# El que fa aquesta funció es llegir el fitxer csv i afegir cada columna en una llista
+# de manera que tots els valors RI estan en una llista Na en una altra llista etc.
+# I finalment retorna una llista on hi ha els valors de la col·lumna type, ja que aqueta no s'ha de 
+# transformar en domini discret. Tambe retorna una llista amb els elements de la primera linea del
+# fitxer csv, el qual sera una llista amb els element RI,Na,Mg,Al,Si,K,Ca,Ba,Fe,Type,  i finalment 
+# tambe retorna una llista de llistes amb els elemens de cada columna en sent aquesta la subllista.
+# exemple [[elements de la col RI][elements de la col Na][elements de la col Mg][]...]
+def openAndRead(css_path):
     with open(css_path) as css:
         RI,Na,Mg,Al,Si,K,Ca,Ba,Fe,Type, atributs= ([] for i in range(11))
         
@@ -99,7 +124,7 @@ def openAndCsstoArff(css_path):
             Ca.append(values[6])
             Ba.append(values[7])
             Fe.append(values[8])
-            Type.append(values[9])
+            Type.append(int(values[9]))
         atributs.append(RI)
         atributs.append(Na)
         atributs.append(Mg)
@@ -109,26 +134,29 @@ def openAndCsstoArff(css_path):
         atributs.append(Ca)
         atributs.append(Ba)
         atributs.append(Fe)
-        atributs.append(Type)
-    
-    return atributs, instances
+    return atributs, instances, Type
 
 
 
         
 if __name__ == '__main__':
-    """if len(sys.argv) != 3:
-        sys.stderr.write("ERROR: Incorrect number of arguments. Given %s. Expected 3.\n" %
-                         len(sys.argv))"""
+    if len(sys.argv) != 5:
+        sys.stderr.write("ERROR: Incorrect number of arguments. Given %s. Expected 4.\n" %
+                         len(sys.argv))
     
-    css_path = sys.argv[1]
-    if not os.path.isfile(css_path):
-        sys.exit("ERROR: CNF file %s does not exist." % css_path)
+    csv_path = sys.argv[1]
+    if not os.path.isfile(csv_path):
+        sys.exit("ERROR: CSV file %s does not exist." % csv_path)
     learnig_path = sys.argv[2]
+    if not os.path.isfile(learnig_path):
+        sys.exit("ERROR: arff file %s does not exist." % learnig_path)
     testing_path = sys.argv[3]
-    length = sys.argv[4]
-    
-    atributs, instances= openAndCsstoArff(css_path)
-    discretList = discreetDomain(atributs,length)
+    if not os.path.isfile(testing_path):
+        sys.exit("ERROR: arff file %s does not exist." % testing_path)
+    argval = sys.argv[4]       
+
+    atributs, instances, Type= openAndRead(csv_path)
+    discretList = discreetDomain(atributs,argval)
+    discretList.append(Type)
     finalDiscretList = orderLists(discretList)
-    classify(finalDiscretList,learnig_path,testing_path,length,instances)
+    split7525(finalDiscretList,learnig_path,testing_path,argval,instances)
